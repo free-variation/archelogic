11f2dd66d91e562a4b4877173a72cdeee76fe6a8
John Stewart <johnstewart@aya.yale.edu>
2017-05-15 01:57:33 -0400

    test script to create dep rels for input sentences in prolog form

--- test/archelogic/corenlp2prolog.clj
+++ test/archelogic/corenlp2prolog.clj
@@ -5,6 +5,23 @@
     
     [clojure.java.io :as io]))
 
-(defn parse-file [file]
-  (with-open [r (io/reader file)]
-    
+(defn parse-file [in-file, out-file]
+  (let [pipe (nlp/make-depparse-pipeline)]
+    (with-open [r (io/reader in-file)]
+      (with-open [w (io/writer out-file)]
+        (doall (map-indexed
+                 (fn [idx sentence]
+                      (let [graph (first (nlp/get-dependency-graphs (nlp/parse-text pipe sentence)))
+                            relations (nlp/flatten-deppgraph graph)]
+                        (.write w "parse(")
+                        (.write w (str idx))
+                        (.write w ",\"")
+                        (.write w sentence)
+                        (.write w "\", [")
+                        (doseq [rel relations]
+                          (.write w (u/sexpr2pred rel))
+                          (.write w ","))
+                        (.write w (str idx))
+                        (.write w "]).\n")))
+                 (line-seq r)))))))
+      
